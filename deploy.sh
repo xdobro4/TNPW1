99f9d87c1636a56f945c53f37cbe6aa069549b0d
Michal Dobrovolný <xdobro4@gmail.com>
2015-02-22 14:16:12 +0100

    deploy oprava

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh root@31.31.76.249 'bash -s' < git-deploy.sh
+ssh root@31.31.76.249 'bash -s' < ./git-deploy.sh $(git log -n 1 --pretty=format:"%H")
--- git-deploy.sh
+++ git-deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 cd /home/vhrb.cz/tnpw1
 git fetch
-LAST_HASH=$(git log -n 1 --pretty=format:"%H")
-#echo $LAST_HASH
-git reset --hard "$LAST_HASH"
+git reset --hard "$1"
