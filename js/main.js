8c67ac674b346dda993a0c0b24a0c535f662115d
Michal Dobrovolný <xdobro4@gmail.com>
2015-02-24 16:01:52 +0100

    mobilní menu - oprava

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
 	// mobile menu
 	$('#mobile-menu-links').on('click', function () {
 		$('nav').slideToggle();
-		if($(this).hasClass('active')) $(this).addClass('active');
-		else $(this).removeClass('active');
+		if(!$(this).hasClass('active')) {
+			$(this).addClass('active');
+		}
+		else {
+			$(this).removeClass('active');
+		}
 	});
 
 
