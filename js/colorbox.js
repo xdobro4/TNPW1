a3ff7ad2a224eee0639e0cec8cc19e40ee407485
Michal Dobrovolný <xdobro4@gmail.com>
2015-03-01 20:00:48 +0100

    oprava contact map

---
+++ js/colorbox.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	// color box
+	$('.project-image').colorbox({
+		rel: 'projects',
+		transition: "fade",
+		width: "90%",
+		height: "90%"
+	});
+
+});
--- js/main.js
+++ js/main.js
@@ -10,12 +10,4 @@ $(document).ready(function() {
 		}
 	});
 
-	// color box
-	$('.project-image').colorbox({
-		rel: 'projects',
-		transition: "fade",
-		width: "90%",
-		height: "90%"
-	});
-
 });
